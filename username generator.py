3a447c8d2ee7465f053970d7c0127350d2a0d593
Arathy <arathyrajeesh2@gmail.com>
2025-09-13 09:50:01 +0530

    add genertor

--- username generator.py
+++ username generator.py
@@ -1,2 +1,19 @@
 #Make a Username Generator from first and last names.
 
+import random
+def generator(first_name,last_name):
+    
+    first_name=first_name.lower()
+    last_name=last_name.lower()
+    
+    first_name=first_name[:3]
+    
+    username = f"{first_name}{last_name}"
+    username+=str(random.randint(10,99))
+    
+    return username
+
+first=str(input('Enter first name: '))
+last=str(input('Enter last name: '))
+username=generator(first,last)
+print(f" username for {first} {last}: {username}")
